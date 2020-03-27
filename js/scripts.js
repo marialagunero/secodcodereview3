02521f7465e76621b42cf8e643f9af4117dfd486
Maria Lagunero <maria58@gmail.com>
2020-03-27 13:59:17 -0700

    add else if statements and return for values in js, code not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
-$(document).ready(function(){
-  $("form#language").submit(function(event){
+$(document).ready(function() {
+  $("form#language").submit(function(event) {
+    event.preventDefault();
     var colorChoice = parseInt($("#colorChoice").val());
     var foodChoice = parseInt($("#foodChoice").val());
     var drinkChoice =parseInt($("#drinkChoice").val());
     var sportChoice = parseInt($("#sportChoice").val());
     var bookChoice = parseInt($("bookChoice").val());
     var result = (colorChoice + foodChoice + drinkChoice + sportChoice + bookChoice)
-
-    if (result <= 1) {
+    console.log();
+    if (result <= 5) {
       $("#ruby").show();
-      $("#csharp, js#").hide();
-    } else 
+      $("#csharp,#js").hide();
+
+    } else if (result <= 15) {  
+      $("#csharp)").show();
+      $("ruby,#js").hide();
+
+    } else if (result <= 25) {
+      $("#js").show();
+      $("#ruby, #csharp").hide();
+
 
     return (result)
 
 
 
 
-  });
+  );
 
 
 
