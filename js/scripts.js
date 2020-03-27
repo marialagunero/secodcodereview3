5ad7cb79d831fdef067b53259f929a1212d94078
Maria Lagunero <maria58@gmail.com>
2020-03-27 14:53:48 -0700

    add missing id to bookChoice value, code works, finally

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,32 @@ $(document).ready(function() {
     event.preventDefault();
     var colorChoice = parseInt($("#colorChoice").val());
     var foodChoice = parseInt($("#foodChoice").val());
-    var drinkChoice =parseInt($("#drinkChoice").val());
+    var drinkChoice = parseInt($("#drinkChoice").val());
     var sportChoice = parseInt($("#sportChoice").val());
-    var bookChoice = parseInt($("bookChoice").val());
+    var bookChoice = parseInt($("#bookChoice").val());
     var result = (colorChoice + foodChoice + drinkChoice + sportChoice + bookChoice)
-    console.log();
+    
     if (result <= 5) {
       $("#ruby").show();
       $("#csharp,#js").hide();
-
-    } else if (result <= 15) {  
-      $("#csharp)").show();
-      $("ruby,#js").hide();
-
-    } else if (result <= 25) {
+      
+    } else if (result <= 10) {  
+      $("#csharp").show();
+      $("#ruby,#js").hide();
+      
+    } else if (result <= 15) {
       $("#js").show();
-      $("#ruby, #csharp").hide();
+      $("#ruby,#csharp").hide();
 
 
     return (result)
 
+    };
 
 
 
-  );
+
+  });
 
 
 
