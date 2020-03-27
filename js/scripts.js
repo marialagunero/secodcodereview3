43f021cca78fa6a6782c3babccf5d193e8089420
Maria Lagunero <maria58@gmail.com>
2020-03-27 11:32:55 -0700

    add variables, values and beginning of if statements in js code not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
   ("form#language").submit(function(event){
+    var colorChoice = parseInt($("#colorChoice").val());
+    var foodChoice = parseInt($("#foodChoice").val());
+    var drinkChoice =parseInt($("#drinkChoice").val());
+    var result = (colorChoice + foodChoice + drinkChoice)
+
+    if (result <= 1) {
+      $("#ruby").show();
+      $("#csharp, js#").hide();
+    } else 
+
+    return (result)
+
+
+
 
   });
 
